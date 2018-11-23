3289dc1508844ebe6ba9480c001828e10efdfd41
Barry Hanna <barryahanna@gmail.com>
2018-11-23 17:09:57 +0000

    Fixed tab order
    Fixed tab order to include the main parts of the page.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -148,7 +148,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
-  // title.setAttribute('tabindex','0');
+  title.setAttribute('tabindex','0');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
@@ -170,6 +170,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+  li.setAttribute('tabindex','0');
   const name = document.createElement('p');
   name.innerHTML = review.name;
   name.classList.add("review-author-name");
